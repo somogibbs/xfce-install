904199ed096d221f43c462f2cb40ed94c20b0a7f
somogibbs <somogibbs@gmail.com>
2025-06-26 15:20:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 !#/bin/bash
 #Packages to install
-sudo apt install blueman synaptic package-update-indicator gnome-disk-utility gparted
-gsmartcontrol hardinfo doublecmd-gtk pdfarranger onboard bleachbit diskscan wireshark guymager uget
+sudo apt install blueman synaptic package-update-indicator gnome-disk-utility gparted gsmartcontrol hardinfo doublecmd-gtk pdfarranger onboard bleachbit diskscan wireshark guymager uget
 
 #Enable Debian backports
 echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware" | sudo tee -a /etc/apt/sources.list
