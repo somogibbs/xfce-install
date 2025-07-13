202fc05c97c475ceb8118b649d67d5c1f00a3ba8
somogibbs <somogibbs@gmail.com>
2025-07-13 05:08:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
-!#/bin/bash
-#Packages to install
-sudo apt install blueman synaptic package-update-indicator gnome-disk-utility gparted gsmartcontrol hardinfo doublecmd-gtk pdfarranger onboard bleachbit diskscan wireshark guymager uget
+#!/bin/bash
+# Debian setup script: packages, UFW, and backports
 
-#Enable Debian backports
+# Add backports repo
 echo "deb http://deb.debian.org/debian bookworm-backports main contrib non-free-firmware" | sudo tee -a /etc/apt/sources.list
 
-#Get qdiskinfo from backport repo
-sudo apt install qdiskinfo
+# Update sources
+sudo apt update
+
+# Install packages
+sudo apt install -y \
+    xfce4 xfce4-goodies blueman synaptic package-update-indicator \
+    gnome-disk-utility gparted gsmartcontrol libavcodec-extra ffmpeg mpv qbittorrent \
+    timeshift openssh-server curl wget lshw stacer cpu-x lm-sensors preload \
+    xfce4-terminal libreoffice-writer firefox-esr obsidian-icon-theme variety \
+    xfce4-power-manager polybar rofi plank ufw
+
+# Setup UFW
+sudo ufw allow ssh
+sudo ufw enable
+
+# Cleanup
+sudo apt autoremove -y
